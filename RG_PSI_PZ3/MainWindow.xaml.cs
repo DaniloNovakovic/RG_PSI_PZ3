644f4d0f1929881adbd22faafe0b7d1e0c1f5dba
Danilo Novakovic <dakenzi97@gmail.com>
2020-05-23 14:08:00 +0200

    Add project files.

---
+++ RG_PSI_PZ3/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RG_PSI_PZ3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Point start = new Point();
+        private Point diffOffset = new Point();
+        private readonly int zoomMax = 7;
+        private int zoomCurent = 1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Viewport_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _viewport.CaptureMouse();
+            start = e.GetPosition(this);
+            diffOffset.X = _translateTransform.OffsetX;
+            diffOffset.Y = _translateTransform.OffsetY;
+        }
+
+        private void Viewport_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _viewport.ReleaseMouseCapture();
+        }
+
+        private void Viewport_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_viewport.IsMouseCaptured)
+            {
+                Point end = e.GetPosition(this);
+                double offsetX = end.X - start.X;
+                double offsetY = end.Y - start.Y;
+                double w = this.Width;
+                double h = this.Height;
+                double translateX = (offsetX * 100) / w;
+                double translateY = -(offsetY * 100) / h;
+                _translateTransform.OffsetX = diffOffset.X + (translateX / (100 * _scaleTransform.ScaleX));
+                _translateTransform.OffsetY = diffOffset.Y + (translateY / (100 * _scaleTransform.ScaleX));
+            }
+        }
+
+        private void Viewport_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            Point p = e.MouseDevice.GetPosition(this);
+            double scaleX = 1;
+            double scaleY = 1;
+            if (e.Delta > 0 && zoomCurent < zoomMax)
+            {
+                scaleX = _scaleTransform.ScaleX + 0.1;
+                scaleY = _scaleTransform.ScaleY + 0.1;
+                zoomCurent++;
+                _scaleTransform.ScaleX = scaleX;
+                _scaleTransform.ScaleY = scaleY;
+            }
+            else if (e.Delta <= 0 && zoomCurent > -zoomMax)
+            {
+                scaleX = _scaleTransform.ScaleX - 0.1;
+                scaleY = _scaleTransform.ScaleY - 0.1;
+                zoomCurent--;
+                _scaleTransform.ScaleX = scaleX;
+                _scaleTransform.ScaleY = scaleY;
+            }
+        }
+    }
+}
